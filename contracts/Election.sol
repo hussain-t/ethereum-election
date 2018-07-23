3fc22651be0c9e4b3f2938dd55726fc50e2b80af
hussain <hussainibnthaj@gmail.com>
2018-07-23 12:44:17 +0530

    validation for vote once and cadidate check

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -27,6 +27,11 @@ contract Election {
     }
 
     function vote(uint _candidateId) public {
+        //check they haven't voted before
+        require(!voters[msg.sender]);
+
+        //require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
         //record that voter has voted
         voters[msg.sender] = true;
         //update candidate vote count;
