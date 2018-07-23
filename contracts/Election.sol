db942c9f4712ac657cc2cbb846074a1c1def03e0
hussain <hussainibnthaj@gmail.com>
2018-07-23 09:40:45 +0530

    Created addCandidate function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,23 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-    //Store candidate
-    //Read candidate
-    string public candidate;
-    //Constructor
-    //Initial smoke test
+    //Model a candida[te
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store candidates
+    //Fetch candidate
+    mapping(uint => Candidate) public candidates;
+    //Store candidates count
+    uint public candidateCount;
+    
     function Election() public {
-        candidate = "Candidate 1";
+    }
+
+    function addCadidate (string _name) private {
+        candidateCount ++;
+        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
     }
 }
