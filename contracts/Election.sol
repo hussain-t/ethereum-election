9ac00045e9e4f6b1494fdd91b1e12558be1beca4
hussain <hussainibnthaj@gmail.com>
2018-07-24 10:40:05 +0530

    Updated front end realime after voting

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-    //Model a candida[te
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
@@ -10,18 +10,18 @@ contract Election {
 
     // Store accounts that have voted
     mapping(address => bool) public voters;
-    //Store candidates
+    // Store Candidates
+    // Fetch Candidate
     mapping(uint => Candidate) public candidates;
-    //Fetch candidate
-    //Store candidates count
+    // Store Candidates Count
     uint public candidatesCount;
 
-    //Voted event
+    // voted event
     event votedEvent (
         uint indexed _candidateId
     );
-    
-    constructor() Election() public {
+
+    function Election () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
@@ -31,18 +31,20 @@ contract Election {
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    function vote(uint _candidateId) public {
-        //check they haven't voted before
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
         require(!voters[msg.sender]);
 
-        //require a valid candidate
+        // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
-        //record that voter has voted
+
+        // record that voter has voted
         voters[msg.sender] = true;
-        //update candidate vote count;
+
+        // update candidate vote Count
         candidates[_candidateId].voteCount ++;
 
-        //trigger event
-        emit votedEvent(_candidateId);
+        // trigger voted event
+        votedEvent(_candidateId);
     }
 }
